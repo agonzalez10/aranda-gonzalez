b01bd3ca6cc1085b7d86555d7b5275bec7e14473
agonzalez10 <53911472+agonzalez10@users.noreply.github.com>
2019-08-25 17:35:01 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -1,38 +1,43 @@
 var trabajos = [{
     figure: "media/trabajo-01-desfile.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de primer trabajo",
-    about: "Texto simulado de descripción de primer trabajo"
+    title: "Emprendimiento estuches",
+    about: "Estuches teñidos y fabricados a mano, 2017"
 }, {
     figure: "media/trabajo-02-app.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo"
+    title: "Yando",
+    about: "Deportes inclusivos, Taller Producto - 2017"
 }, {
     figure: "media/trabajo-03-homy.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
-    about: "Texto simulado de descripción de tercer trabajo"
+    title: "Packaging sustentable para flores ",
+    about: "Trabajo con microemprendedoras de Maipú, Taller Calidad I - 2019"
 }, {
     figure: "media/trabajo-04-editorial.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de cuarto trabajo",
-    about: "Texto simulado de descripción de cuarto trabajo"
+    title: "Experimentación con cerámica",
+    about: "Cerámica y modelado, 2017"
 }, {
     figure: "media/trabajo-05-artesania.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de quinto trabajo",
-    about: "Texto simulado de descripción de quinto trabajo"
+    title: "Marra la tiendita",
+    about: "Trabajo con microemprendedoras de Maipú, Taller Calidad I - 2019"
 }, {
     figure: "media/trabajo-06-textil.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de sexto trabajo",
-    about: "Texto simulado de descripción de sexto trabajo"
+    title: "Banca Zig-zag",
+    about: "Banca a base de terciado 3mm y amarracables, Estructura y materiales - 2019"
 }, {
     figure: "media/trabajo-07-materiales.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
+    title: "Zapatitos de papel",
+    about: "Trabajo con papel maché, 2017"
+  }, {
+      figure: "media/trabajo-08-juego.jpg",
+      alt: "Descripción de la imagen",
+      title: "Cuadrálisis",
+      about: "Creación juego de mesa, Taller Producto - 2018"
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
